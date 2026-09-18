02d95726b28971f9bc8828858aa923842708dea2
s6906022610401-alt <s6906022610401@kmutnb.ac.th>
2026-09-18 21:21:43 +0700

    hw4

--- GroupBySignature.py
+++ GroupBySignature.py
@@ -1,13 +1,26 @@
 def group_by_signature(words: list) -> list:
-    pass
+    groups = {}
+
+    for word in words:
+        if not word or not word.isalpha() or not word.islower():
+            continue
+
+        signature = "".join(sorted(word))
+
+        if signature not in groups:
+            groups[signature] = []
+        groups[signature].append(word)
+
+    return list(groups.values())
+
 
 if __name__ == "__main__":
     # Example 1
     words = ["abc", "bca", "cab", "bac", "xyz", "yxz", "zxy", "dog"]
     print(group_by_signature(words))
-    # Output: [["abc", "bca", "cab", "bac"], ["xyz", "yxz", "zxy"], ["dog"]]
+    # Output: [['abc', 'bca', 'cab', 'bac'], ['xyz', 'yxz', 'zxy'], ['dog']]
 
     # Example 2
     words = ["apple", "pale", "leap", "plea", "papel", "hello"]
     print(group_by_signature(words))
-    # Output: [["apple", "papel"], ["pale", "leap", "plea"], ["hello"]]
+    # Output: [['apple', 'papel'], ['pale', 'leap', 'plea'], ['hello']]
